29d7251c7292c66c813b2da0028df4b23049adb8
Rob <robflettcc@gmail.com>
2017-04-05 17:34:55 +0100

    2 tests passing

---
+++ array_tasks.js
@@ -0,0 +1,41 @@
+var arrayTasks = {
+
+	concat: function (arr1, arr2) {
+		for (var i=0; i < arr2.length; i++) {
+			arr1.push( arr2[i]);
+		}
+		return arr1;
+	},
+
+	insertAt: function (arr, itemToAdd, index) {
+		arr.splice(index, 0, itemToAdd);
+		return arr;
+	},
+
+	// square: function (arr) {
+		
+	// },
+
+	// sum: function (arr) {
+		
+	// },
+
+	// findDuplicates: function (arr) {
+		
+	// },
+
+	// removeAndClone: function (arr, valueToRemove) {
+		
+	// },
+
+	// findIndexesOf: function (arr, itemToFind) {
+		
+	// },
+
+	// sumOfAllEvenNumbersSquared: function (arr) {
+		
+	// }
+
+}
+
+module.exports = arrayTasks
